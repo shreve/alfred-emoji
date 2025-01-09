348e33176d26eb6861697de47e206cb2d0e4d7e2
Violet Shreve <shreve@umich.edu>
2025-01-08 21:35:55 -0500

    Double quote variables in build.sh to prevent splitting

--- build.sh
+++ build.sh
@@ -2,23 +2,23 @@
 set -e
 
 # this script's parent directory
-cd $(dirname $0)
+cd "$(dirname $0)"
 parentDir=$(pwd)
 
 [ ! -d output ] && mkdir output
 cd output
 
-cp ${parentDir}/src/info.plist.xml ./info.plist
+cp "${parentDir}"/src/info.plist.xml ./info.plist
 
 echo "Generating icons ..."
 [ ! -d icons ] && mkdir icons
 cd icons
-node ${parentDir}/lib/genicons.js
+node "${parentDir}"/lib/genicons.js
 cd ..
 cp icons/beer_mug.png ./icon.png
 
 echo "Creating emoji pack ..."
-cd ${parentDir}
+cd "${parentDir}"
 node ./lib/genpack.js
 
 echo "Generating jxa compatible source ..."
@@ -35,8 +35,8 @@ sed -i '' -e "/{{readme}}/{r ${readme}" -e 'd' -e '}' info.plist
 
 echo "Injecting auto-update script ..."
 update="$(mktemp)"
-cat ${parentDir}/src/update.sh | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g' > ${update}
+cat "${parentDir}"/src/update.sh | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g' > ${update}
 sed -i '' -e "/{{update_script}}/{r ${update}" -e 'd' -e '}' info.plist
 
 echo "Bundling workflow ..."
-zip -Z deflate -rq9 ${parentDir}/alfred-emoji.alfredworkflow * -x etc
+zip -Z deflate -rq9 "${parentDir}"/alfred-emoji.alfredworkflow * -x etc
